4ec7680ce81faad488ab7150d41fa6a56895cec7
hoc911 <hocine1212007.h@gmail.com>
2025-01-14 21:06:40 +0100

    Create crew.py

---
+++ crew.py
@@ -0,0 +1,20 @@
+# crew.py
+from custom_agent import get_gemini_response
+
+# تمرير النص فقط بدلاً من كائن الاستجابة بالكامل
+from crewai import Crew,Process
+from tasks import programer_task
+from agents import programer
+
+## Forming the tech focused crew with some enhanced configuration
+crew=Crew(
+    agents=[programer],
+    tasks=[programer_task],
+    process=Process.sequential,
+    verbose=True  # or verbose=False
+)
+
+## starting the task execution process wiht enhanced feedback
+
+# هنا يتم تمرير النص من الاستجابة
+# نتيجة التشغيل ستكون في ملف index.html
